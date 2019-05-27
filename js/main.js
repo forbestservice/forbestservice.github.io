2b3ce3eb1d8a71e56fa7b7be51ce85da22b9e5e4
heibai <15958107325@163.com>
2019-05-27 11:13:02 +0800

    修改信息

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
 jQuery(function($) {
-	"use strict";
+  "use strict";
 
-	$.validate();
+  $.validate();
 
-	new WOW().init();
-	//Variables
-	var fixed_point = 0;
-	var loaded = false;
+  new WOW().init();
+  //Variables
+  var fixed_point = 0;
+  var loaded = false;
 
-	// Author Code Here
-	$(window).load(function(){
-		loaded = true;
-		Adjust();
-	});
+  // Author Code Here
+  $(window).load(function() {
+    loaded = true;
+    Adjust();
+  });
 
-	$(window).resize(function(){
-		Adjust();
-	});
+  $(window).resize(function() {
+    Adjust();
+  });
 
-	function Adjust(){
-		if($(window).width() > 768)
-		{
-			$('header').height($(window).height());
-			$('.intro-book').css('top', ($('header').height() / 2 - $('.intro-book').height() / 2) + "px");
-			$('.intro-text').css('top', ($('header').height() / 2 - $('.intro-text').height() / 2) + "px");
-		}
-		else {
-			$('header').height(400);
-		}
-	}
+  function Adjust() {
+    $("header").height($(window).height());
+    $(".intro-book").css(
+      "top",
+      $("header").height() / 2 - $(".intro-book").height() / 2 + "px"
+    );
+    $(".intro-text").css(
+      "top",
+      $("header").height() / 2 - $(".intro-text").height() / 2 + "px"
+    );
+  }
 });
